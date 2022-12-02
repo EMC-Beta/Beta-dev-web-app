b91e31df9238b3548fdd4cf226cb4f71311e184b
michael.bowen <michael.bowen@mymail.champlain.edu>
2022-12-02 16:01:58 -0500

    changed webgL index file

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (<>
     
-        <iframe width={'100%'} height={'1000'} src={'/beta-app/prototype-3/WebGL/index.html'}/>
+        <iframe width={'100%'} height={'1000'} src={'/beta-app/prototype-3/WebGL/WebGL/index.html'}/>
 {/*        <iframe width={'100%'} height={'1000'} src={'/beta-app/prototype-2/WebGL/index.html'}/>*/}
    
       </>
